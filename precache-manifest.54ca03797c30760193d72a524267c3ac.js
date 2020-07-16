2564f17fec8cd6b2aec0dc91cf93083f5d064867
kvaso <aakvasovaa@gmail.com>
2020-07-16 15:44:12 +0300

    Updates

---
+++ precache-manifest.54ca03797c30760193d72a524267c3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f92cbc14877170b79a8b86a850c70ce",
+    "url": "/test-app-contactlist/index.html"
+  },
+  {
+    "revision": "214872535d2c64476fd2",
+    "url": "/test-app-contactlist/static/css/main.8ed8b1d4.chunk.css"
+  },
+  {
+    "revision": "34569a3130ce5ffe6af9",
+    "url": "/test-app-contactlist/static/js/2.3e5502e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-app-contactlist/static/js/2.3e5502e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214872535d2c64476fd2",
+    "url": "/test-app-contactlist/static/js/main.69a6c992.chunk.js"
+  },
+  {
+    "revision": "7a35986376fb61e40c1d",
+    "url": "/test-app-contactlist/static/js/runtime-main.4b4d5ff7.js"
+  }
+]);
